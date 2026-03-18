bc90f3cd064ef78617d6d9d614a127f57513ca9d
Yogesh Joshi <yjoshi.techark@gmail.com>
2026-03-18 14:26:21 +0530

    chore: configure TypeScript and ESLint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    ignores: ['src/__tests__/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': 'error'
+    }
+  }
+];
